28de7d6d361c25148f5a5c2f6f7e4b8da582d77e
homulily85 <157974079+homulily85@users.noreply.github.com>
2024-10-19 03:40:00 +0700

    fix edit result view

---
+++ main_app/EditResultView.py
@@ -0,0 +1,16 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views import View
+from django.contrib import messages
+from .models import Subject, Staff, Student, StudentResult
+
+class EditResultView(View):
+    def get(self, request, *args, **kwargs):
+        resultForm = EditResultForm()
+        staff = get_object_or_404(Staff, admin=request.user)
+        resultForm.fields['subject'].queryset = Subject.objects.filter(staff=staff)
+        context = {
+            'form': resultForm,
+            'page_title': "Edit Student's Result"
+        }
+        return render(request, "staff_template/edit_student_result.html", context)
+
