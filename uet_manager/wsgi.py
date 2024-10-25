eea544ee7c334a47d8df8171da3ba1114a6102f3
Dun <61546385+dungchivas722@users.noreply.github.com>
2024-10-25 07:06:00 +0700

    them wsgi.py

--- uet_manager/wsgi.py
+++ uet_manager/wsgi.py
@@ -2,7 +2,3 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_management_system.settings')
-
-application = get_wsgi_application()
