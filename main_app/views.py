1cd97a76d98ec67a3a02097e576f61115fb8a578
homulily85 <157974079+homulily85@users.noreply.github.com>
2024-10-26 20:16:00 +0700

    create urls

---
+++ main_app/views.py
@@ -0,0 +1,42 @@
+import json
+import requests
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.views.decorators.csrf import csrf_exempt
+
+from .EmailBackend import EmailBackend
+from .models import Attendance, Session, Subject
+
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+        if request.user.user_type == '1':
+            return redirect(reverse("admin_home"))
+        elif request.user.user_type == '2':
+            return redirect(reverse("staff_home"))
+        else:
+            return redirect(reverse("student_home"))
+    return render(request, 'main_app/login.html')
+
+
+def doLogin(request, **kwargs):
+    if request.method != 'POST':
+        return HttpResponse("<h4>Denied</h4>")
+    else:
+        # Authenticate
+        user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
+        if user is not None:
+            login(request, user)
+            if user.user_type == '1':
+                return redirect(reverse("admin_home"))
+            elif user.user_type == '2':
+                return redirect(reverse("staff_home"))
+            else:
+                return redirect(reverse("student_home"))
+        else:
+            messages.error(request, "Invalid details")
+            return redirect("/")
+
