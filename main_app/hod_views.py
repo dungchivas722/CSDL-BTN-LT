d6b698c9ea89c590bac0c34bfdc183f6f46b9f36
homulily85 <157974079+homulily85@users.noreply.github.com>
2024-10-26 10:00:00 +0700

    create hod_views

---
+++ main_app/hod_views.py
@@ -0,0 +1,196 @@
+import json
+import requests
+from django.contrib import messages
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import (HttpResponse, HttpResponseRedirect,
+                              get_object_or_404, redirect, render)
+from django.templatetags.static import static
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import UpdateView
+
+from .forms import *
+from .models import *
+
+
+def admin_home(request):
+    total_staff = Staff.objects.all().count()
+    total_students = Student.objects.all().count()
+    subjects = Subject.objects.all()
+    total_subject = subjects.count()
+    total_course = Course.objects.all().count()
+    attendance_list = Attendance.objects.filter(subject__in=subjects)
+    total_attendance = attendance_list.count()
+    attendance_list = []
+    subject_list = []
+    for subject in subjects:
+        attendance_count = Attendance.objects.filter(subject=subject).count()
+        subject_list.append(subject.name[:7])
+        attendance_list.append(attendance_count)
+    context = {
+        'page_title': "Bảng điều khiển quản trị",
+        'total_students': total_students,
+        'total_staff': total_staff,
+        'total_course': total_course,
+        'total_subject': total_subject,
+        'subject_list': subject_list,
+        'attendance_list': attendance_list
+
+    }
+    return render(request, 'hod_template/home_content.html', context)
+
+
+def add_staff(request):
+    form = StaffForm(request.POST or None, request.FILES or None)
+    context = {'form': form, 'page_title': 'Thêm Giảng Viên'}
+    if request.method == 'POST':
+        if form.is_valid():
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            address = form.cleaned_data.get('address')
+            email = form.cleaned_data.get('email')
+            gender = form.cleaned_data.get('gender')
+            password = form.cleaned_data.get('password')
+            course = form.cleaned_data.get('course')
+            passport = request.FILES.get('profile_pic')
+            fs = FileSystemStorage()
+            filename = fs.save(passport.name, passport)
+            passport_url = fs.url(filename)
+            try:
+                user = CustomUser.objects.create_user(
+                    email=email, password=password, user_type=2, first_name=first_name, last_name=last_name, profile_pic=passport_url)
+                user.gender = gender
+                user.address = address
+                user.staff.course = course
+                user.save()
+                messages.success(request, "Successfully Added")
+                return redirect(reverse('add_staff'))
+
+            except Exception as e:
+                messages.error(request, "Could Not Add " + str(e))
+        else:
+            messages.error(request, "Please fulfil all requirements")
+
+    return render(request, 'hod_template/add_staff_template.html', context)
+
+
+def add_student(request):
+    student_form = StudentForm(request.POST or None, request.FILES or None)
+    context = {'form': student_form, 'page_title': 'Thêm sinh viên'}
+    if request.method == 'POST':
+        if student_form.is_valid():
+            first_name = student_form.cleaned_data.get('first_name')
+            last_name = student_form.cleaned_data.get('last_name')
+            address = student_form.cleaned_data.get('address')
+            email = student_form.cleaned_data.get('email')
+            gender = student_form.cleaned_data.get('gender')
+            password = student_form.cleaned_data.get('password')
+            course = student_form.cleaned_data.get('course')
+            session = student_form.cleaned_data.get('session')
+            passport = request.FILES['profile_pic']
+            fs = FileSystemStorage()
+            filename = fs.save(passport.name, passport)
+            passport_url = fs.url(filename)
+            try:
+                user = CustomUser.objects.create_user(
+                    email=email, password=password, user_type=3, first_name=first_name, last_name=last_name, profile_pic=passport_url)
+                user.gender = gender
+                user.address = address
+                user.student.session = session
+                user.student.course = course
+                user.save()
+                messages.success(request, "Successfully Added")
+                return redirect(reverse('add_student'))
+            except Exception as e:
+                messages.error(request, "Could Not Add: " + str(e))
+        else:
+            messages.error(request, "Could Not Add: ")
+    return render(request, 'hod_template/add_student_template.html', context)
+
+
+def add_course(request):
+    form = CourseForm(request.POST or None)
+    context = {
+        'form': form,
+        'page_title': 'Thêm chương trình học'
+    }
+    if request.method == 'POST':
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            try:
+                course = Course()
+                course.name = name
+                course.save()
+                messages.success(request, "Successfully Added")
+                return redirect(reverse('add_course'))
+            except:
+                messages.error(request, "Could Not Add")
+        else:
+            messages.error(request, "Could Not Add")
+    return render(request, 'hod_template/add_course_template.html', context)
+
+
+def add_subject(request):
+    form = SubjectForm(request.POST or None)
+    context = {
+        'form': form,
+        'page_title': 'Thêm môn học'
+    }
+    if request.method == 'POST':
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            course = form.cleaned_data.get('course')
+            staff = form.cleaned_data.get('staff')
+            try:
+                subject = Subject()
+                subject.name = name
+                subject.staff = staff
+                subject.course = course
+                subject.save()
+                messages.success(request, "Successfully Added")
+                return redirect(reverse('add_subject'))
+
+            except Exception as e:
+                messages.error(request, "Could Not Add " + str(e))
+        else:
+            messages.error(request, "Fill Form Properly")
+
+    return render(request, 'hod_template/add_subject_template.html', context)
+
+
+def manage_staff(request):
+    allStaff = CustomUser.objects.filter(user_type=2)
+    context = {
+        'allStaff': allStaff,
+        'page_title': 'Quản lý giảng viên'
+    }
+    return render(request, "hod_template/manage_staff.html", context)
+
+
+def manage_student(request):
+    students = CustomUser.objects.filter(user_type=3)
+    context = {
+        'students': students,
+        'page_title': 'Quản lý sinh viên'
+    }
+    return render(request, "hod_template/manage_student.html", context)
+
+
+def manage_course(request):
+    courses = Course.objects.all()
+    context = {
+        'courses': courses,
+        'page_title': 'Quản lý chương trình học'
+    }
+    return render(request, "hod_template/manage_course.html", context)
+
+
+def manage_subject(request):
+    subjects = Subject.objects.all()
+    context = {
+        'subjects': subjects,
+        'page_title': 'Quản lý môn học'
+    }
+    return render(request, "hod_template/manage_subject.html", context)
+
