7908e7d64e304f0a99bd490e0335e5adba79d77e
homulily85 <157974079+homulily85@users.noreply.github.com>
2024-12-15 14:06:00 +0700

    update EditResultView.py

--- main_app/EditResultView.py
+++ main_app/EditResultView.py
@@ -2,6 +2,9 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.views import View
 from django.contrib import messages
 from .models import Subject, Staff, Student, StudentResult
+from .forms import EditResultForm
+from django.urls import reverse
+
 
 class EditResultView(View):
     def get(self, request, *args, **kwargs):
@@ -14,3 +17,24 @@ class EditResultView(View):
         }
         return render(request, "staff_template/edit_student_result.html", context)
 
+    def post(self, request, *args, **kwargs):
+        form = EditResultForm(request.POST)
+        context = {'form': form, 'page_title': "Edit Student's Result"}
+        if form.is_valid():
+            try:
+                student = form.cleaned_data.get('student')
+                subject = form.cleaned_data.get('subject')
+                test = form.cleaned_data.get('test')
+                exam = form.cleaned_data.get('exam')
+                # Validating
+                result = StudentResult.objects.get(student=student, subject=subject)
+                result.exam = exam
+                result.test = test
+                result.save()
+                messages.success(request, "Result Updated")
+                return redirect(reverse('edit_student_result'))
+            except Exception as e:
+                messages.warning(request, "Result Could Not Be Updated")
+        else:
+            messages.warning(request, "Result Could Not Be Updated")
+        return render(request, "staff_template/edit_student_result.html", context)
